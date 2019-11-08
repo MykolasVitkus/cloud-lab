9e129c0884a2c154065d978620ba28ad79e9616b
Mykolas <mykolasvitkus@gmail.com>
2019-11-08 14:49:14 +0200

    v0.2

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"https://github.com/MykolasVitkus/cloud-lab/blob/master/internal/hashes/hashes.go"
 )
 
 func Sha256Handler(w http.ResponseWriter, r *http.Request) {
 	// to get the username, use the following:
-	//username := mux.Vars(r)["username"]
+	username := mux.Vars(r)["username"]
 
+	sha256string := GetHash("Sha256", username)
 	// to calculate sha256 hash of a string, use internal/hashes package and function GetHash
 
 	// to send the response, use the following:
-	// fmt.Fprint(w, "your response here")
+	fmt.Fprint(w, sha256string)
 
 	w.WriteHeader(http.StatusOK)
 }
